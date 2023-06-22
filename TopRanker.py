d02d9c18df350c23aa3c27f446e5fe7fd48a3b6b
anri <anri.kezeroti.1@btu.edu.ge>
2023-06-22 16:33:03 +0400

    refactor, solve 1 bug new bug inteoduced by new data structure

--- TopRanker.py
+++ TopRanker.py
@@ -1,14 +1,15 @@
-class TopFiveRanker:
-    def __init__(self):
-        self.data = [(None, None, None, None, None, 0.0)] * 6
+class TopRanker:
+    def __init__(self, n):
+        self.data = [(None, None, None, None, None, 0.0)] * (n + 1)
 
     def update_if_greater(self, score, is_greeater):
-        pointer = 5
+        pointer = len(self.data) - 1
         self.data[pointer] = score
         if pointer > 0 and is_greeater(self.data[pointer], self.data[pointer - 1]):
             upcast_score(self.data, pointer, is_greeater)
 
     def get_data(self):
+        self.data.pop()
         return self.data
 
 
@@ -21,8 +22,11 @@ def upcast_score(list, index, is_greater):
         list[index], list[index - 1] = list[index - 1], list[index]
         index -= 1
 
-
-# data_structure = TopFiveRanker()
+# def is_greater(item1, item2):
+#     return item1[5] > item2[5]
+#
+#
+# data_structure = TopRanker(5)
 # data = [(None, None, None, None, None, 0.1),
 #         (None, None, None, None, None, 0.2),
 #         (None, None, None, None, None, 0.5),
@@ -36,4 +40,4 @@ def upcast_score(list, index, is_greater):
 # for i in data:
 #     data_structure.update_if_greater(i, is_greater)
 #
-# print(data_structure.get_data())
+# print(len(data_structure.get_data()))
--- semantic_search.py
+++ semantic_search.py
@@ -1,14 +1,15 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-from TopFiveRanker import TopFiveRanker
+
+from TopRanker import TopRanker
 
 
 def semantic_search(query, products, top_k=3):
     model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
     query_embedding = model.encode([query])[0]
 
-    result_data_structure = TopFiveRanker()
+    result_data_structure = TopRanker(5)
 
     for row in products:
         phone_id, name, brand, specifications, price, embedding_data = row
